4a1b7f398c1b3af2f89953d524955c3c4d12b0ba
andras <vagner.andras@gb-art.hu>
2017-03-23 16:22:41 +0100

    build start

--- server.js
+++ server.js
@@ -1,42 +1,21 @@
-var cool = require('cool-ascii-faces');
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+'use strict';
 
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
 
-// arrays
-users = [];
-connections = [];
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
 
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
+const io = socketIO(server);
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-    response.render('pages/index')
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-app.get('/cool', function(request, response) {
-    response.send(cool());
-});
-
-app.get('/easteregg', function(request, response) {
-    var result = '';
-    var times = process.env.TIMES || 5;
-    for (i=0; i < times; i++)
-        result += i + ' ';
-    response.send(result);
-});
-
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-});
-
-
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
